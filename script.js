16128e9aec2d5b1c9ab8d942fb664dcfc4477e80
YusufAkilevi <yusufaklevi@gmail.com>
2023-03-22 17:22:39 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const adviceText = document.querySelector(".advice-text");
+const adviceNumber = document.querySelector(".advice-number");
+const diceBtn = document.querySelector(".icon-dice-button");
+
+const getAdvice = async function () {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const { slip } = await res.json();
+
+  adviceNumber.textContent = `ADVICE #${slip.id}`;
+  adviceText.textContent = slip.advice;
+};
+
+diceBtn.addEventListener("click", function () {
+  getAdvice();
+});
