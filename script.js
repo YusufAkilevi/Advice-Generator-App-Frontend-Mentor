cd3b08d7cca85dab0694a44dc72f40e1df0f9c13
YusufAkilevi <yusufaklevi@gmail.com>
2023-03-23 18:10:18 +0300

    Final commit- Readme and error handling added

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ const adviceNumber = document.querySelector(".advice-number");
 const diceBtn = document.querySelector(".icon-dice-button");
 
 const getAdvice = async function () {
-  const res = await fetch("https://api.adviceslip.com/advice");
-  const { slip } = await res.json();
+  try {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const { slip } = await res.json();
 
-  adviceNumber.textContent = `ADVICE #${slip.id}`;
-  adviceText.textContent = `"${slip.advice}"`;
+    adviceNumber.textContent = `ADVICE #${slip.id}`;
+    adviceText.textContent = `"${slip.advice}"`;
+  } catch (err) {
+    adviceText.textContent = `${err} Something went wrong!`;
+  }
 };
 getAdvice();
 diceBtn.addEventListener("click", function () {
